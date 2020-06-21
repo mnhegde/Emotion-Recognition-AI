d441a0f1a0b11f6a323d751c19a2a618ab729203
Pierre <Lessardpw@gmail.com>
2020-06-21 17:55:45 -0400

    batch integrations

--- model_functions.py
+++ model_functions.py
@@ -33,13 +33,50 @@ def open_model():
         return(model)
 
 def overwrite_model_file(model):
+    #do not mess with
     with open('model.pkl', 'wb') as output:
         pickle.dump(model, output, pickle.HIGHEST_PROTOCOL)
         print('succesfully trained')
 
+def save_batch_data(data,emotion):
+    with open ('batch_data_x.txt', 'rb') as fp:
+        batch_data_x = pickle.load(fp)
+
+    batch_data_x.append(data)
+
+    with open ('batch_data_y.txt', 'rb') as fp:
+        batch_data_y = pickle.load(fp)
+
+    batch_data_y.append(emotion)
+
+    with open('batch_data_x.txt', 'wb') as fp:
+        pickle.dump(batch_data_x, fp)
+    
+    with open('batch_data_y.txt', 'wb') as fp:
+        pickle.dump(batch_data_y, fp)
+
+def reset_batch_data():
+    batch_data_x = []
+    batch_data_y = []
+
+    with open('batch_data_x.txt', 'wb') as fp:
+        pickle.dump(batch_data_x, fp)
+    
+    with open('batch_data_y.txt', 'wb') as fp:
+        pickle.dump(batch_data_y, fp)
+
+def open_batch_data(xy):
+    if xy == 'x':
+        with open ('batch_data_x.txt', 'rb') as fp:
+            batch_data_x = pickle.load(fp)
+            return(batch_data_x)
+    if xy == 'y':
+        with open ('batch_data_y.txt', 'rb') as fp:
+            batch_data_y = pickle.load(fp)
+            return(batch_data_y)
+
 def model_predict(file_name):
     extracted_features = [extract_feature(file_name, mfcc=True, chroma=True, mel=True)]
-    print(extracted_features)
     model = open_model()
     
     model.batch_size = 256
@@ -49,17 +86,20 @@ def model_predict(file_name):
 
 
 def model_train(file_name, emotion):
-    extracted_features = [extract_feature(file_name, mfcc=True, chroma=True, mel=True)]
     
-    correct_emotion = [emotion]
+    extracted_features = extract_feature(file_name, mfcc=True, chroma=True, mel=True)
+    save_batch_data(extracted_features, emotion)
 
-    model = open_model()
-    
-    model.fit(extracted_features,correct_emotion)
+    batch_data_y = open_batch_data('y')
+    batch_data_x = open_batch_data('x')
 
-    overwrite_model_file(model)
+    if len(batch_data_y) == 2017:
+        model = open_model()
+        model.fit(batch_data_x,batch_data_y)
+        overwrite_model_file(model)
 
-chosen_file = "sample_tests/pierre_confused.wav"
+
+chosen_file = "sample_tests/calm_no_inflection.wav"
 emotion = 'angry'
 print(model_predict(chosen_file)[0])
 #model_train(chosen_file, emotion)
